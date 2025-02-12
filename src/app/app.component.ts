b2e7b59cb55208c4dffbc9ff86b23c33af70726c
Léo Gouchon <leo.gouchon@gmail.com>
2025-02-12 10:48:14 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PrimeNG} from 'primeng/config';
+import {DockModule} from 'primeng/dock';
+import {NavigationComponent} from './components/navigation/navigation.component';
+import {Card} from 'primeng/card';
+import {Panel} from 'primeng/panel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+    imports: [RouterOutlet, DockModule, NavigationComponent, Card, Panel],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'squashappfrontend';
+export class AppComponent implements OnInit {
+  title = 'Squash';
+
+  constructor(private primeng: PrimeNG) {}
+
+  ngOnInit() {
+    this.primeng.ripple.set(true);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {AddMatchComponent} from './components/add-match/add-match.component';
+import {HistoricComponent} from './components/historic/historic.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "add-match",
+        component: AddMatchComponent
+    },
+    {
+        path: "historic",
+        component: HistoricComponent
+    }
+];
