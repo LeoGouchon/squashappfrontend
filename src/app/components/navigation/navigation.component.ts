a83d62866db5921d0ab18cc6c85b2c2ae213b8d2
Léo Gouchon <leo.gouchon@gmail.com>
2025-05-09 15:24:06 +0200

    feat(menu): change ux of the menu and make it clearer

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,25 +1,27 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, HostListener, Inject, OnInit} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
-import {Menubar} from 'primeng/menubar';
 import {ConfirmationService, MenuItem, MessageService} from 'primeng/api';
 import {NavigationService, NavigationServiceInterface} from '../../services/navigation.service';
 import {TokenService} from '../../services/token.service';
-import {Avatar} from 'primeng/avatar';
-import {OverlayBadge} from 'primeng/overlaybadge';
 import {Toast} from 'primeng/toast';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import {AppRoutes} from '../../AppRoutes';
+import {PanelMenu} from 'primeng/panelmenu';
+import {Fluid} from 'primeng/fluid';
+import {Divider} from 'primeng/divider';
+import {Drawer} from 'primeng/drawer';
 
 
 @Component({
     selector: 'app-navigation',
     imports: [
         ReactiveFormsModule,
-        Menubar,
-        Avatar,
-        OverlayBadge,
         Toast,
-        ConfirmDialogModule
+        ConfirmDialogModule,
+        PanelMenu,
+        Fluid,
+        Divider,
+        Drawer
     ],
     templateUrl: './navigation.component.html',
     styleUrl: './navigation.component.css',
@@ -30,6 +32,8 @@ import {AppRoutes} from '../../AppRoutes';
     ]
 })
 export class NavigationComponent implements OnInit {
+    protected isMobile = false;
+    protected displaySidebar = false;
 
     constructor(
         @Inject('NavigationServiceInterface') protected readonly navigation: NavigationServiceInterface,
@@ -42,9 +46,12 @@ export class NavigationComponent implements OnInit {
 
     items: MenuItem[] | undefined;
 
+    profilItems: MenuItem[] | undefined;
+
     ngOnInit() {
         this.items = [
             {
+                styleClass: "menu-item",
                 label: 'Nouveau Match',
                 icon: 'pi pi-plus',
                 command: () => {
@@ -77,6 +84,52 @@ export class NavigationComponent implements OnInit {
                 }
             },
         ];
+
+        this.profilItems = [
+            {
+                label: 'Mon compte',
+                icon: 'pi pi-user',
+                command: () => {
+                    this.tokenService.getAccessToken()
+                        ?
+                        this.navigation.navigateTo(AppRoutes.PROFILE)
+                        :
+                        this.messageService.add({
+                            severity: 'error',
+                            summary: 'Déconnecté',
+                            detail: "Vous n'êtes pas connecté",
+                            life: 3000
+                        });
+                }
+            },
+            {
+                label: 'Deconnexion',
+                icon: 'pi pi-sign-out',
+                command: () => {
+                    this.tokenService.getAccessToken()
+                        ?
+                        this.navigation.navigateTo(AppRoutes.LOGIN)
+                        :
+                        this.messageService.add({
+                            severity: 'error',
+                            summary: 'Déconnecté',
+                            detail: "Vous n'êtes pas connecté",
+                            life: 3000
+                        });
+                }
+            },
+        ];
+
+        this.checkScreenSize();
+    }
+
+    @HostListener('window:resize')
+    checkScreenSize() {
+        this.isMobile = window.innerWidth < 768;
+    }
+
+    toggleSidebar() {
+        this.displaySidebar = !this.displaySidebar;
     }
 
     protected readonly AppRoutes = AppRoutes;
