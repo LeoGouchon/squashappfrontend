20e7c935d8d8d04ebac4e7a81f7363da9beedc9b
Léo Gouchon <leo.gouchon@gmail.com>
2025-06-30 23:39:21 +0200

    fix(api): fix to the new api hubscore 1.0.0 version

--- generate-env.js
+++ generate-env.js
@@ -22,5 +22,6 @@ fs.writeFile(envFilePath, targetEnvironment, function (err) {
         console.error('❌ Erreur lors de la création de environment.prod.ts', err);
     } else {
         console.log('✅ Fichier environment.prod.ts généré avec succès');
+        console.log('Using API URL:', this.apiUrl);
     }
 });
